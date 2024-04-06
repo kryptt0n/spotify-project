da5844ba4130ab0805a51efdbe39471f2e7e432d
Chak Pu Patrick Tong <tongchakpupatrick@gmail.com>
2024-04-06 14:20:19 -0400

    Create sample File which shows all the Genre

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const clientld = ``;
-const clientSecret = ``;
-let accessToken;
+const clientld = `30691c2e47bf4fe4ad77188b788c3906`;
+const clientSecret = `39b59e348d5c4985b66e1242d5dc4f05`;
+const $ = (selector) => {
+    const element = document.querySelector(selector);
+    return {
+        html: () => element.innerHTML,
+        text: () => element.textContent,
+        append: (child) => element.appendChild(child),
+        addClass: (className) => element.classList.add(className),
+        toggleClass: (className) => element.classList.toggle(className),
+    };
+};
+
+
+const getGenre  = async () => {
+    const accessToken = await getToken();
+    const response = await fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    
+      const data = await response.json();
+      return data.genres;
+    }
 
 const getToken = async () => {
 const result = await fetch("https://accounts.spotify.com/api/token",
@@ -13,5 +35,16 @@ const result = await fetch("https://accounts.spotify.com/api/token",
     });
 
     const data = await result.json();
-    accessToken = data.access_token;
+    const accessToken = data.access_token;
+    return accessToken;
 };
+const displayGenres = async () => {
+    const genres = await getGenre();
+    genres.forEach(genre => 
+    {
+        let li = document.createElement('li');
+        li.innerHTML =`<li class='genre-item'>${genre}</li>`;
+        $('#genres-list').append(li);
+    });
+}
+displayGenres()
