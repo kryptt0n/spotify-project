a1b7cdd6f3240b59f6ff1034f636d3701cb195a2
Chak Pu Patrick Tong <tongchakpupatrick@gmail.com>
2024-04-07 23:15:03 -0400

    Create search bar and buttons from html

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 const clientld = `30691c2e47bf4fe4ad77188b788c3906`;
 const clientSecret = `39b59e348d5c4985b66e1242d5dc4f05`;
-const $ = (selector) => {
-    const element = document.querySelector(selector);
-    return {
-        html: () => element.innerHTML,
-        text: () => element.textContent,
-        append: (child) => element.appendChild(child),
-        addClass: (className) => element.classList.add(className),
-        toggleClass: (className) => element.classList.toggle(className),
-    };
-};
 
 
 const getGenre  = async () => {
@@ -38,12 +28,30 @@ const result = await fetch("https://accounts.spotify.com/api/token",
     const accessToken = data.access_token;
     return accessToken;
 };
-const displayGenres = async () => {
-    const genres = await getGenre();
+const getPlaylist = async (genre) => {
+    const accessToken = await getToken();
+    const response = await fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    
+      const data = await response.json();
+      return data.genres;
+    }
+
+function reset(){
+    document.getElementById('genres-list').innerHTML = "";
+    displayGenres(getGenre());
+}
+
+const displayGenres = async (method) => {
+    const genres = await method;
     let counter = 0;
     genres.forEach(genre => 
     {
         let li = document.createElement('li');
+        let playlist = 
         li.innerHTML =`
         <li class='genre-item dropdown'>
             <button class="btn btn-m w-100 btn-light dropdown-toggle mr-9" type="button" id="dropdownMenuButton${counter}" data-bs-toggle="dropdown" aria-expanded="false">
@@ -54,7 +62,8 @@ const displayGenres = async () => {
             </ul>
         </li>
         `;
-        $('#genres-list').append(li);
+        document.getElementById('genres-list').append(li);
     });
 }
-displayGenres()
+window.onload = displayGenres(getGenre());
+document.getElementById('reset-btn').addEventListener('click',reset)
