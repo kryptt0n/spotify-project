069a5614a2a36c45ceabb0d52aa9a8dcb90180a1
Chak Pu Patrick Tong <tongchakpupatrick@gmail.com>
2024-04-08 13:26:54 -0400

    Create method handleLazyLoading to optimize loading process, also created local storage to deal with error 429

--- script.js
+++ script.js
@@ -1,76 +1,155 @@
-const clientId = `e8c1d0e9976d47c99dd1dbdb27d22038`;
-const clientSecret = `b63cf10ea15543a182723cad0c07fee2`;
+const clientId = `30691c2e47bf4fe4ad77188b788c3906`;
+const clientSecret = `39b59e348d5c4985b66e1242d5dc4f05`;
 
 const getGenre = async () => {
+    try {
+        const accessToken = await getToken();
+        const response = await fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        });
+
+        if (response.status === 429) {
+            const savedData = localStorage.getItem('spotify_genres');
+            if (savedData) {
+                return JSON.parse(savedData).genres;
+            } else {
+                console.error('Rate limit exceeded, no saved data found.');
+            }
+        }
+
+        if (!response.ok) {
+            console.error(`Failed to fetch genre. Error Code: ${response.status}`);
+        }
+
+        const data = await response.json();
+        localStorage.setItem('spotify_genres', JSON.stringify(data));
+        return data.genres;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+const getToken = async () => {
+    try {
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: "Basic " + btoa(`${clientId}:${clientSecret}`),
+            },
+            body: "grant_type=client_credentials",
+        });
+
+        if (!response.ok) {
+            console.error(`Failed to fetch token. Error Code: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.access_token;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+const getPlaylist = async(genre) =>{
     const accessToken = await getToken();
-    const response = await fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${genre}&type=playlist`, {
         headers: {
             Authorization: 'Bearer ' + accessToken
         }
     });
-
+    if (!response.ok) {
+        alert(`Failed to fetch playlist Error Code:${response.status}`);
+    }
     const data = await response.json();
-    return data.genres;
-};
-
-const getToken = async () => {
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: "Basic " + btoa(`${clientId}:${clientSecret}`),
-        },
-        body: "grant_type=client_credentials",
+    return data.playlists.items;
+}
+const getPlaylistName = async (list) =>{ 
+    let nameList = []
+    list.forEach(element => {
+        nameList.push(element.name)
     });
+    return nameList;
+}
 
-    const data = await result.json();
-    const accessToken = data.access_token;
-    return accessToken;
+const getSavedData = (key) => {
+    const savedData = localStorage.getItem(key);
+    if (savedData) {
+        return JSON.parse(savedData);
+    }
+    return null;
 };
 
 const reset = async () => {
     document.getElementById('genres-list').innerHTML = "";
-    displayGenres(getGenre());
-}
+    displayGenres(await getGenre());
+};
 
 const searchGenres = async () => {
     let keyword = document.getElementById('search-bar').value.toLowerCase();
     let genreList = await getGenre();
-    let filteredGenres = [];
+    let searchedGenres = [];
 
     genreList.forEach(genre => {
         if (genre.match(keyword)) {
-            filteredGenres.push(genre);
+            searchedGenres.push(genre);
         }
     });
-
+    if (!searchedGenres.length > 0){
+        alert("No result found")
+    }
+    searchedGenres.sort();
     document.getElementById('genres-list').innerHTML = "";
-    displayGenres(filteredGenres);
-}
+    displayGenres(searchedGenres.sort());
+};
 
-const displayGenres = async (method) => {
-    const genres = await method;
-    let counter = 0;
-    genres.forEach(genre => {
-        let li = document.createElement('li');
-        li.innerHTML = `
-        <li class='genre-item dropdown'>
-            <button class="btn btn-m w-100 btn-light dropdown-toggle mr-9" type="button" id="dropdownMenuButton${counter}" data-bs-toggle="dropdown" aria-expanded="false">
-                ${genre}
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton${counter++}">
-                <li></li>
-            </ul>
-        </li>
-        `;
-        document.getElementById('genres-list').append(li);
+
+const displayGenres = async (data) => {
+    const genres = await data;
+        genres.forEach(async (genre, index) => {
+            let li = document.createElement('li');
+            li.innerHTML = `
+            <li class='genre-item dropdown'>
+                <button class="btn btn-m w-100 btn-light dropdown-toggle mr-9" type="button" id="dropdownMenuButton${index}" data-bs-toggle="dropdown" aria-expanded="false">
+                    ${genre}
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton${index}"></ul>
+            </li>`;
+            document.getElementById('genres-list').appendChild(li);
+        });
+};
+
+const handleLazyLoading = async () => {
+    const genresList = document.getElementById('genres-list');
+    const genres = genresList.querySelectorAll('.genre-item');
+
+    genres.forEach(async (genreElement) => {
+            const genreName = genreElement.querySelector('button').textContent.toLowerCase();
+            const playlistElement = genreElement.querySelector('ul');
+
+            if (!playlistElement.hasChildNodes()) {
+
+                    const playlists = await getPlaylist(genreName);
+                    const playlistNames = await getPlaylistName(playlists);
+
+
+                    playlistNames.forEach(name => {
+                        const listItem = document.createElement('li');
+                        listItem.textContent = name;
+                        playlistElement.appendChild(listItem);
+                    });
+            }
     });
 };
 
-window.onload = () => {
-    displayGenres(getGenre());
+window.onload = async () => {
+    displayGenres(await getGenre());
 };
 
 document.getElementById('reset-btn').addEventListener('click', reset);
+window.addEventListener('click', handleLazyLoading);
 
 document.getElementById('search-btn').addEventListener('click', searchGenres);
